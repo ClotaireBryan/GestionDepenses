4926f9f041292688f63478cea18aecff8426e2bf
Bryan <clotaire.bryan@gmail.com>
2018-08-05 15:39:23 +0200

    - ajout des activiter "Ajouter" - "Lister" - "Consulter compte" - mise en liens des boutons vers les activiter

---
+++ app/src/main/java/clotaire/bryan/gestiondepenses/AjouterPageActivity.java
@@ -0,0 +1,13 @@
+package clotaire.bryan.gestiondepenses;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AjouterPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter_page);
+    }
+}
--- app/src/main/java/clotaire/bryan/gestiondepenses/MainActivity.java
+++ app/src/main/java/clotaire/bryan/gestiondepenses/MainActivity.java
@@ -1,7 +1,9 @@
 package clotaire.bryan.gestiondepenses;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickAjoutPage(View view) {
+        Intent intent = new Intent(this, AjouterPageActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickListerDepensesPage(View view) {
+        Intent intent = new Intent(this, ListerPageActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickSoldesComptePage(View view) {
+        Intent intent = new Intent(this, ConsulterPageActivity.class);
+        startActivity(intent);
     }
 }

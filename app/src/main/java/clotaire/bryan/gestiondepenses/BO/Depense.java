6a24934b32a83ee7a60a98e0d9e233005417dd56
Bryan <clotaire.bryan@gmail.com>
2018-08-05 23:37:53 +0200

    ajout du recycler view pour les liste dans le menu lister ( a modifer de tel façon avoir cette liste de depense dans une liste de mois

--- app/src/main/java/clotaire/bryan/gestiondepenses/BO/Depense.java
+++ app/src/main/java/clotaire/bryan/gestiondepenses/BO/Depense.java
@@ -1,28 +1,31 @@
 package clotaire.bryan.gestiondepenses.BO;
 
 import java.util.Date;
-import java.util.UUID;
 
 public class Depense {
 
-    int id;
-    int montant;
-    Date date;
-    String lieu;
-    String ville;
+    private int id;
+    private double montant;
+    private  Date date;
+    private  String lieu;
+    private  String ville;
 
     public Depense() {
         super();
     }
 
-    public Depense(int id, int montant, String lieu, String ville) {
+    public Depense(int id, double montant, String lieu, String ville) {
         this.id = id;
         this.montant = montant;
         this.lieu = lieu;
         this.ville = ville;
     }
+    public Depense(String lieu, String ville) {
+        this.lieu = lieu;
+        this.ville = ville;
+    }
 
-    public Depense(int id, int montant, String lieu) {
+    public Depense(int id, double montant, String lieu) {
         this.id = id;
         this.montant = montant;
         this.lieu = lieu;
@@ -36,11 +39,11 @@ public class Depense {
         this.id = id;
     }
 
-    public int getMontant() {
+    public double getMontant() {
         return montant;
     }
 
-    public void setMontant(int montant) {
+    public void setMontant(double montant) {
         this.montant = montant;
     }
 
